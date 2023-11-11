0a4b2bc95f55b8a37d53855f8f1df21a764f56f5
tmathew777 <tmathe22@my.centennialcollege.ca>
2023-11-11 17:43:20 -0500

    sell-used-products-site

---
+++ controllers/categoryController.js
@@ -0,0 +1,69 @@
+const Category = require('../models/categoryModel');
+
+
+const createCategory = async (req, res) => {
+  try {
+    const newCategory = new Category(req.body);
+    const savedCategory = await newCategory.save();
+    res.status(201).json(savedCategory);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const getCategoryById = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const updateCategoryById = async (req, res) => {
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedCategory) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+    res.json(updatedCategory);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const deleteCategoryById = async (req, res) => {
+  try {
+    const deletedCategory = await Category.findByIdAndRemove(req.params.id);
+    if (!deletedCategory) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+    res.json(deletedCategory);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
+};
---
+++ controllers/productController.js
@@ -0,0 +1,69 @@
+const Product = require('../models/productModel');
+
+
+const createProduct = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const updateProductById = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+const deleteProductById = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndRemove(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json(deletedProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+};
